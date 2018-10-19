0677d6571395d393ee26c9d2cbd9f9168b32b0f5
mKleinCreative <miketheklein@gmail.com>
2018-10-19 23:39:48 +0000

    set up prototypes for mandatory functions and struct for formatting

---
+++ holberton.h
@@ -0,0 +1,16 @@
+#ifndef _HOLBERTON_H_
+#define _HOLBERTON_H_
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+int _printf(const char *format, ...);
+typedef struct formatter {
+	char * symbol;
+	void (*f)(char *);
+} format_me;
+void (*converter(char *))(va_list list);
+void print_char(va_list list);
+void print_string(va_list list);
+void print_d(va_list list);
+void print_i(va_list list);
+#endif
