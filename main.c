c798fb45d197a1ab9ae8b6d7cb45ea27260ee5e5
bdbaraban <375@holbertonschool.com>
2018-10-21 02:52:35 +0000

    Included test for %%

--- main.c
+++ main.c
@@ -16,6 +16,10 @@ int main(void)
 	printf("Character:[%c]\n", 'H');
 	_printf("String:[%s]\n", "I am a string !");
 	printf("String:[%s]\n", "I am a string !");
+	len = _printf("Percent:[%%]\n");
+	len2 = printf("Percent:[%%]\n");
+	_printf("Len:[%d]\n", len);
+	printf("Len:[%d]\n", len2);
 
 	return (0);
 }
