7fbe0633e5ab75a262ceaa8666956d4895b75613
mKleinCreative <miketheklein@gmail.com>
2018-10-23 17:30:15 +0000

    saving flag start for later

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
  * _printf - does wonderful things
  * @format: Character string to print - may contain directives.
@@ -9,31 +8,35 @@
 int _printf(const char *format, ...)
 {
 	va_list args;
-	char *buffer, *buffer_ptr;
-	int i, ret = 0, add = 1;
+	char *buffer, *buffer_ptr, flag_list[12`];
+	int i, j, ret = 0, add = 1, end, flag_len;
 
 	buffer = malloc(sizeof(char) * 1024);
 	if (buffer == NULL)
 		exit(1);
 	buffer_ptr = buffer;
-
 	va_start(args, format);
 	for (i = 0; format && *(format + i); i++)
 	{
 		if (*(format + i) == '%')
 		{
-			if (*(format + i + 1) == '\0' ||
-			    converter(format + i + 1))
+			j = 0;
+			for (end = j; format[end] != " " && format[end] != '\0'; ++end)
 			{
-				i++;
-				if (converter(format + i))
+				flag_list[j] = &format[i + 1];
+				if (*(format + i + j) == '\0' ||
+			    		converter(format + i + j))
 				{
-					add = converter(format + i)(args, buffer);
-					ret += add;
-					buffer += add;
-
-					if (*(format + i + 1) != '%')
-						i++;
+					i++;
+					j++;
+					if (converter(format + i))
+					{
+						add = converter(format + i)(args, buffer);
+						ret += add;
+						buffer += add;
+						if (*(format + i + 1) != '%')
+							i++;
+					}
 				}
 			}
 		}
@@ -42,10 +45,8 @@ int _printf(const char *format, ...)
 		buffer++;
 	}
 	*buffer = '\0';
-
 	write(1, buffer_ptr, ret);
 	va_end(args);
 	free(buffer_ptr);
-
 	return (ret);
 }
