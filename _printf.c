a213e6e37276675b654790f03e5cf9716a32e015
bdbaraban <375@holbertonschool.com>
2018-12-30 00:53:27 +0000

    Implement tmp variable to skip over length modifiers provided without specifiers

--- _printf.c
+++ _printf.c
@@ -27,6 +27,19 @@ unsigned char count_one_bits(unsigned char num)
 	return (count);
 }
 
+
+/**
+ * cleanup - Peforms cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
+ */
+void cleanup(va_list args, buffer_t *output)
+{
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
+
 /**
  * _printf - Outputs a formatted string.
  * @format: Character string to print - may contain directives.
@@ -37,7 +50,7 @@ int _printf(const char *format, ...)
 {
 	buffer_t *output;
 	va_list args;
-	int i, ret = 0;
+	int i, tmp, ret = 0;
 	unsigned char flag, len;
 	unsigned int (*f)(va_list, buffer_t *, unsigned char, unsigned char);
 
@@ -50,29 +63,29 @@ int _printf(const char *format, ...)
 	va_start(args, format);
 	for (i = 0; *(format + i); i++)
 	{
+		len = 0;
 		if (*(format + i) == '%')
 		{
-			if (*(format + i + 1) == '\0')
+			flag = handle_flags(format + i + 1);
+			tmp = count_one_bits(flag);
+			len = handle_length(format + i + tmp + 1);
+			tmp += (len != 0) ? 1 : 0;
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flag, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
 			{
 				ret = -1;
 				break;
 			}
-			flag = handle_flags(format + i + 1);
-			i += count_one_bits(flag);
-			len = handle_length(format + i + 1);
-			i += (len != 0) ? 1 : 0;
-			f = handle_specifiers(format + i + 1);
-			if (f != NULL)
-			{
-				i++;
-				ret += f(args, output, flag, len);
-				continue;
-			}
 		}
 		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
 	}
-	va_end(args);
-	write(1, output->start, output->len);
-	free_buffer(output);
+	cleanup(args, output);
 	return (ret);
 }
